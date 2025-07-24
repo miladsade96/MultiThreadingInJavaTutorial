0899099498d722ed5b3b7569c240c6464b9e61da
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-07-24 22:44:35 +0330

    Add loop to main method for printing numbers from 0 to 20

--- src/main/java/part1/NormalJavaWithoutMultiThreading.java
+++ src/main/java/part1/NormalJavaWithoutMultiThreading.java
@@ -1,7 +1,15 @@
 package part1;
 
 public class NormalJavaWithoutMultiThreading {
+
     public static void main(String[] args) {
 
+        for (int i = 0; i < 11; i++) {
+            System.out.println("i is: " + i);
+        }
+
+        for (int i = 11; i < 21; i++) {
+            System.out.println("i is: " + i);
+        }
     }
 }
